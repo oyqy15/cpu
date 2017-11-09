ff4f5336237d8c09ce5b09d67be3a8b674a40e6a
oyqy15 <767233324@qq.com>
2017-11-09 19:32:56 +0800

    initial

---
+++ pc.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    19:32:22 11/09/2017 
+-- Design Name: 
+-- Module Name:    pc - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity pc is
+    Port ( rst : in  STD_LOGIC;
+           clk : in  STD_LOGIC);
+end pc;
+
+architecture Behavioral of pc is
+
+begin
+
+
+end Behavioral;
+
